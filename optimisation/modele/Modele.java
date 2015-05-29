60aac7b3b68bd191d840771abec40a5918133b8a
Aurélien <aurel.andre@yahoo.fr>
2015-05-29 14:44:21 +0200

    Ajout methode initialise dans Modele

--- optimisation/modele/Modele.java
+++ optimisation/modele/Modele.java
@@ -3,6 +3,16 @@ package optimisation.modele;
 import java.util.Observable;
 
 public class Modele  extends Observable implements Runnable{
+	
+	protected boolean initialise;
+
+	public boolean isInitialise() {
+		return initialise;
+	}
+
+	public void setInitialise(boolean initialise) {
+		this.initialise = initialise;
+	}
 
 	public void miseAJour() {
 		// TODO Auto-generated method stub
--- optimisation/vue/VueClavier.java
+++ optimisation/vue/VueClavier.java
@@ -1,13 +1,16 @@
 package optimisation.vue;
 
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import optimisation.modele.Modele;
+import repCo.controleur.EcouteurBoutonLabyrinthe;
 
 public class VueClavier extends JPanel implements Observer{
 	
@@ -25,6 +28,20 @@ public class VueClavier extends JPanel implements Observer{
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
+		if(m.isInitialise()){
+			this.removeAll();
+            this.setLayout(new GridLayout(m.getHauteur(), m.getLargeur(), 0, 0));
+            tabButton = new JButton[m.getHauteur()][m.getLargeur()];
+            for (int i = 0; i < tabButton.length; i++) {
+				for (int j = 0; j < tabButton[0].length; j++) {
+					tabButton[i][j] = new JButton();
+					this.add(tabButton[i][j]);
+					tabButton[i][j].addActionListener(new EcouteurBoutonLabyrinthe(m, i, j));
+					tabButton[i][j].setContentAreaFilled(false);
+            		tabButton[i][j].setFocusPainted(false);
+				}
+			}
+            m.setInitialise(false);
+        }
     }
 }
